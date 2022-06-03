442d7d8d445f1395969a5d9f1ac2208f1b29517e
Emre Can <emrecanoner@eskisehir.edu.tr>
2022-06-03 14:06:13 +0300

    Initial commit

---
+++ üniversitesınav/frmUniSinavHesap.Designer.cs
@@ -0,0 +1,381 @@
+﻿namespace üniversitesınav
+{
+    partial class frmUniSinavHesap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btntürk = new System.Windows.Forms.Button();
+            this.tboxt_t = new System.Windows.Forms.TextBox();
+            this.tboxt_f = new System.Windows.Forms.TextBox();
+            this.tboxt_e = new System.Windows.Forms.TextBox();
+            this.tboxm_t = new System.Windows.Forms.TextBox();
+            this.tboxm_f = new System.Windows.Forms.TextBox();
+            this.tboxm_e = new System.Windows.Forms.TextBox();
+            this.tboxf_t = new System.Windows.Forms.TextBox();
+            this.tboxf_f = new System.Windows.Forms.TextBox();
+            this.tboxf_e = new System.Windows.Forms.TextBox();
+            this.btnmat = new System.Windows.Forms.Button();
+            this.btnfen = new System.Windows.Forms.Button();
+            this.btnsos = new System.Windows.Forms.Button();
+            this.tboxs_t = new System.Windows.Forms.TextBox();
+            this.tboxs_f = new System.Windows.Forms.TextBox();
+            this.tboxs_e = new System.Windows.Forms.TextBox();
+            this.btngenel = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblgenel = new System.Windows.Forms.Label();
+            this.lblnettürk = new System.Windows.Forms.Label();
+            this.lblnetmat = new System.Windows.Forms.Label();
+            this.lblnetfen = new System.Windows.Forms.Label();
+            this.lblnetsos = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btntürk
+            // 
+            this.btntürk.Location = new System.Drawing.Point(497, 32);
+            this.btntürk.Name = "btntürk";
+            this.btntürk.Size = new System.Drawing.Size(75, 23);
+            this.btntürk.TabIndex = 0;
+            this.btntürk.Text = "HESAPLA";
+            this.btntürk.UseVisualStyleBackColor = true;
+            this.btntürk.Click += new System.EventHandler(this.btntürk_Click);
+            // 
+            // tboxt_t
+            // 
+            this.tboxt_t.Location = new System.Drawing.Point(132, 32);
+            this.tboxt_t.Name = "tboxt_t";
+            this.tboxt_t.Size = new System.Drawing.Size(100, 20);
+            this.tboxt_t.TabIndex = 1;
+            // 
+            // tboxt_f
+            // 
+            this.tboxt_f.Location = new System.Drawing.Point(251, 32);
+            this.tboxt_f.Name = "tboxt_f";
+            this.tboxt_f.Size = new System.Drawing.Size(100, 20);
+            this.tboxt_f.TabIndex = 2;
+            // 
+            // tboxt_e
+            // 
+            this.tboxt_e.Location = new System.Drawing.Point(369, 32);
+            this.tboxt_e.Name = "tboxt_e";
+            this.tboxt_e.Size = new System.Drawing.Size(100, 20);
+            this.tboxt_e.TabIndex = 3;
+            // 
+            // tboxm_t
+            // 
+            this.tboxm_t.Location = new System.Drawing.Point(132, 69);
+            this.tboxm_t.Name = "tboxm_t";
+            this.tboxm_t.Size = new System.Drawing.Size(100, 20);
+            this.tboxm_t.TabIndex = 4;
+            // 
+            // tboxm_f
+            // 
+            this.tboxm_f.Location = new System.Drawing.Point(251, 69);
+            this.tboxm_f.Name = "tboxm_f";
+            this.tboxm_f.Size = new System.Drawing.Size(100, 20);
+            this.tboxm_f.TabIndex = 5;
+            // 
+            // tboxm_e
+            // 
+            this.tboxm_e.Location = new System.Drawing.Point(369, 69);
+            this.tboxm_e.Name = "tboxm_e";
+            this.tboxm_e.Size = new System.Drawing.Size(100, 20);
+            this.tboxm_e.TabIndex = 6;
+            // 
+            // tboxf_t
+            // 
+            this.tboxf_t.Location = new System.Drawing.Point(132, 106);
+            this.tboxf_t.Name = "tboxf_t";
+            this.tboxf_t.Size = new System.Drawing.Size(100, 20);
+            this.tboxf_t.TabIndex = 7;
+            // 
+            // tboxf_f
+            // 
+            this.tboxf_f.Location = new System.Drawing.Point(251, 106);
+            this.tboxf_f.Name = "tboxf_f";
+            this.tboxf_f.Size = new System.Drawing.Size(100, 20);
+            this.tboxf_f.TabIndex = 8;
+            // 
+            // tboxf_e
+            // 
+            this.tboxf_e.Location = new System.Drawing.Point(369, 106);
+            this.tboxf_e.Name = "tboxf_e";
+            this.tboxf_e.Size = new System.Drawing.Size(100, 20);
+            this.tboxf_e.TabIndex = 9;
+            // 
+            // btnmat
+            // 
+            this.btnmat.Location = new System.Drawing.Point(497, 67);
+            this.btnmat.Name = "btnmat";
+            this.btnmat.Size = new System.Drawing.Size(75, 23);
+            this.btnmat.TabIndex = 10;
+            this.btnmat.Text = "HESAPLA";
+            this.btnmat.UseVisualStyleBackColor = true;
+            this.btnmat.Click += new System.EventHandler(this.btnmat_Click);
+            // 
+            // btnfen
+            // 
+            this.btnfen.Location = new System.Drawing.Point(497, 103);
+            this.btnfen.Name = "btnfen";
+            this.btnfen.Size = new System.Drawing.Size(75, 23);
+            this.btnfen.TabIndex = 11;
+            this.btnfen.Text = "HESAPLA";
+            this.btnfen.UseVisualStyleBackColor = true;
+            this.btnfen.Click += new System.EventHandler(this.btnfen_Click);
+            // 
+            // btnsos
+            // 
+            this.btnsos.Location = new System.Drawing.Point(497, 140);
+            this.btnsos.Name = "btnsos";
+            this.btnsos.Size = new System.Drawing.Size(75, 23);
+            this.btnsos.TabIndex = 12;
+            this.btnsos.Text = "HESAPLA";
+            this.btnsos.UseVisualStyleBackColor = true;
+            this.btnsos.Click += new System.EventHandler(this.btnsos_Click);
+            // 
+            // tboxs_t
+            // 
+            this.tboxs_t.Location = new System.Drawing.Point(132, 143);
+            this.tboxs_t.Name = "tboxs_t";
+            this.tboxs_t.Size = new System.Drawing.Size(100, 20);
+            this.tboxs_t.TabIndex = 13;
+            // 
+            // tboxs_f
+            // 
+            this.tboxs_f.Location = new System.Drawing.Point(251, 143);
+            this.tboxs_f.Name = "tboxs_f";
+            this.tboxs_f.Size = new System.Drawing.Size(100, 20);
+            this.tboxs_f.TabIndex = 14;
+            // 
+            // tboxs_e
+            // 
+            this.tboxs_e.Location = new System.Drawing.Point(369, 143);
+            this.tboxs_e.Name = "tboxs_e";
+            this.tboxs_e.Size = new System.Drawing.Size(100, 20);
+            this.tboxs_e.TabIndex = 15;
+            // 
+            // btngenel
+            // 
+            this.btngenel.Location = new System.Drawing.Point(737, 31);
+            this.btngenel.Name = "btngenel";
+            this.btngenel.Size = new System.Drawing.Size(107, 58);
+            this.btngenel.TabIndex = 16;
+            this.btngenel.Text = "HESAPLA";
+            this.btngenel.UseVisualStyleBackColor = true;
+            this.btngenel.Click += new System.EventHandler(this.btngenel_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(48, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "TÜRKÇE";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "MATEAMATİK";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(60, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 13);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "FEN";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(48, 150);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "SOSYAL";
+            // 
+            // lblgenel
+            // 
+            this.lblgenel.AutoSize = true;
+            this.lblgenel.Location = new System.Drawing.Point(758, 108);
+            this.lblgenel.Name = "lblgenel";
+            this.lblgenel.Size = new System.Drawing.Size(68, 13);
+            this.lblgenel.TabIndex = 21;
+            this.lblgenel.Text = "GENEL NET";
+            // 
+            // lblnettürk
+            // 
+            this.lblnettürk.AutoSize = true;
+            this.lblnettürk.Location = new System.Drawing.Point(594, 37);
+            this.lblnettürk.Name = "lblnettürk";
+            this.lblnettürk.Size = new System.Drawing.Size(29, 13);
+            this.lblnettürk.TabIndex = 22;
+            this.lblnettürk.Text = "NET";
+            // 
+            // lblnetmat
+            // 
+            this.lblnetmat.AutoSize = true;
+            this.lblnetmat.Location = new System.Drawing.Point(594, 72);
+            this.lblnetmat.Name = "lblnetmat";
+            this.lblnetmat.Size = new System.Drawing.Size(29, 13);
+            this.lblnetmat.TabIndex = 23;
+            this.lblnetmat.Text = "NET";
+            // 
+            // lblnetfen
+            // 
+            this.lblnetfen.AutoSize = true;
+            this.lblnetfen.Location = new System.Drawing.Point(594, 109);
+            this.lblnetfen.Name = "lblnetfen";
+            this.lblnetfen.Size = new System.Drawing.Size(29, 13);
+            this.lblnetfen.TabIndex = 24;
+            this.lblnetfen.Text = "NET";
+            this.lblnetfen.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // lblnetsos
+            // 
+            this.lblnetsos.AutoSize = true;
+            this.lblnetsos.Location = new System.Drawing.Point(594, 143);
+            this.lblnetsos.Name = "lblnetsos";
+            this.lblnetsos.Size = new System.Drawing.Size(29, 13);
+            this.lblnetsos.TabIndex = 25;
+            this.lblnetsos.Text = "NET";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(173, 13);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(15, 13);
+            this.label5.TabIndex = 26;
+            this.label5.Text = "D";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(296, 13);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(14, 13);
+            this.label6.TabIndex = 27;
+            this.label6.Text = "Y";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(411, 13);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(14, 13);
+            this.label7.TabIndex = 28;
+            this.label7.Text = "B";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // frmUniSinavHesap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(918, 209);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblnetsos);
+            this.Controls.Add(this.lblnetfen);
+            this.Controls.Add(this.lblnetmat);
+            this.Controls.Add(this.lblnettürk);
+            this.Controls.Add(this.lblgenel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btngenel);
+            this.Controls.Add(this.tboxs_e);
+            this.Controls.Add(this.tboxs_f);
+            this.Controls.Add(this.tboxs_t);
+            this.Controls.Add(this.btnsos);
+            this.Controls.Add(this.btnfen);
+            this.Controls.Add(this.btnmat);
+            this.Controls.Add(this.tboxf_e);
+            this.Controls.Add(this.tboxf_f);
+            this.Controls.Add(this.tboxf_t);
+            this.Controls.Add(this.tboxm_e);
+            this.Controls.Add(this.tboxm_f);
+            this.Controls.Add(this.tboxm_t);
+            this.Controls.Add(this.tboxt_e);
+            this.Controls.Add(this.tboxt_f);
+            this.Controls.Add(this.tboxt_t);
+            this.Controls.Add(this.btntürk);
+            this.Name = "frmUniSinavHesap";
+            this.Text = "Üniversitesi Sınav Sonucu Hesaplama";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btntürk;
+        private System.Windows.Forms.TextBox tboxt_t;
+        private System.Windows.Forms.TextBox tboxt_f;
+        private System.Windows.Forms.TextBox tboxt_e;
+        private System.Windows.Forms.TextBox tboxm_t;
+        private System.Windows.Forms.TextBox tboxm_f;
+        private System.Windows.Forms.TextBox tboxm_e;
+        private System.Windows.Forms.TextBox tboxf_t;
+        private System.Windows.Forms.TextBox tboxf_f;
+        private System.Windows.Forms.TextBox tboxf_e;
+        private System.Windows.Forms.Button btnmat;
+        private System.Windows.Forms.Button btnfen;
+        private System.Windows.Forms.Button btnsos;
+        private System.Windows.Forms.TextBox tboxs_t;
+        private System.Windows.Forms.TextBox tboxs_f;
+        private System.Windows.Forms.TextBox tboxs_e;
+        private System.Windows.Forms.Button btngenel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblgenel;
+        private System.Windows.Forms.Label lblnettürk;
+        private System.Windows.Forms.Label lblnetmat;
+        private System.Windows.Forms.Label lblnetfen;
+        private System.Windows.Forms.Label lblnetsos;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ üniversitesınav/frmUniSinavHesap.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace üniversitesınav
+{
+    public partial class frmUniSinavHesap : Form
+    {
+        public frmUniSinavHesap()
+        {
+            InitializeComponent();
+        }
+        double t_dogru = 0;
+        double t_yanlis = 0;
+        double t_bos = 0;
+        double m_dogru = 0;
+        double m_yanlis = 0;
+        double m_bos = 0;
+        double f_dogru = 0;
+        double f_yanlis = 0;
+        double f_bos = 0;
+        double s_dogru = 0;
+        double s_yanlis = 0;
+        double s_bos = 0;
+        double t_ort = 0;
+        double m_ort = 0;
+        double s_ort = 0;
+        double f_ort = 0;
+        double g_net = 0;
+
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btntürk_Click(object sender, EventArgs e)
+        {
+            t_dogru = Convert.ToDouble(tboxt_t.Text);
+            t_yanlis = Convert.ToDouble(tboxt_f.Text);
+            t_bos = Convert.ToDouble(tboxt_e.Text);
+            Convert.ToDouble(t_dogru);
+            Convert.ToDouble(t_yanlis);
+            Convert.ToDouble(t_bos);
+            t_ort = (t_dogru - (t_yanlis / 4));
+            lblnettürk.Text = Convert.ToString(t_ort);
+            if((t_dogru+t_yanlis+t_bos)>40)
+            {
+                MessageBox.Show("Fazla Girdiniz", "Uyarı", MessageBoxButtons.OK);
+
+            }
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnmat_Click(object sender, EventArgs e)
+        {
+            m_dogru = Convert.ToDouble(tboxm_t.Text);
+            m_yanlis = Convert.ToDouble(tboxm_f.Text);
+            m_bos = Convert.ToDouble(tboxm_e.Text);
+            Convert.ToDouble(m_dogru);
+            Convert.ToDouble(m_yanlis);
+            Convert.ToDouble(m_bos);
+            m_ort = (m_dogru - (m_yanlis / 4));
+            lblnetmat.Text = Convert.ToString(m_ort);
+            if ((f_dogru + f_yanlis + f_bos) > 40)
+            {
+                MessageBox.Show("Fazla Girdiniz", "Uyarı", MessageBoxButtons.OK);
+
+            }
+        }
+
+        private void btnfen_Click(object sender, EventArgs e)
+        {
+            f_dogru = Convert.ToDouble(tboxf_t.Text);
+            f_yanlis = Convert.ToDouble(tboxf_f.Text);
+            f_bos = Convert.ToDouble(tboxf_e.Text);
+            Convert.ToDouble(f_dogru);
+            Convert.ToDouble(f_yanlis);
+            Convert.ToDouble(f_bos);
+            f_ort = (f_dogru - (f_yanlis / 4));
+            lblnetfen.Text = Convert.ToString(f_ort);
+            if ((f_dogru + f_yanlis + f_bos) > 40)
+            {
+                MessageBox.Show("Fazla Girdiniz", "Uyarı", MessageBoxButtons.OK);
+
+            }
+        }
+
+        private void btnsos_Click(object sender, EventArgs e)
+        {
+            s_dogru = Convert.ToDouble(tboxs_t.Text);
+            s_yanlis = Convert.ToDouble(tboxs_f.Text);
+            s_bos = Convert.ToDouble(tboxs_e.Text);
+            Convert.ToDouble(s_dogru);
+            Convert.ToDouble(s_yanlis);
+            Convert.ToDouble(s_bos);
+            s_ort = (s_dogru - (s_yanlis / 4));
+            lblnetsos.Text = Convert.ToString(s_ort);
+            if ((s_dogru + s_yanlis + s_bos) > 40)
+            {
+                MessageBox.Show("Fazla Girdiniz", "Uyarı", MessageBoxButtons.OK);
+
+            }
+        }
+
+        private void btngenel_Click(object sender, EventArgs e)
+        {
+            g_net = (t_ort + f_ort + m_ort + s_ort);
+            lblgenel.Text = Convert.ToString(g_net);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Her branş için 40 soru üzerinden değerlendiriniz.", "UYARI", MessageBoxButtons.OK);
+        }
+    }
+}
